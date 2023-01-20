ad218e4cbbb172f56898b9736c2b5f37abdc14da
Jerimiah Milton <jerimiah.milton@gmail.com>
2023-01-19 22:59:20 -0600

    Added bookings page

---
+++ app/javascript/components/Bookings.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Time</th>
+                <th scope="col">Name</th>
+                <th scope="col">Phone Number</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">8:00 AM</th>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">8:30 AM</th>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">9:00 AM</th>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">9:30 AM</th>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">10:00 AM</th>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">10:30 AM</th>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">11:00 AM</th>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">11:30 AM</th>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">12:00 AM</th>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">12:30 AM</th>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">1:00 PM</th>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">1:30 PM</th>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">2:00 PM</th>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">2:30 PM</th>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">3:00 PM</th>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">3:30 PM</th>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+);
